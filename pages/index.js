adf46cc9c89aa4581d7a759dce4174ec90857350
Rashed Ahmed <rashed.ahmed@email.com>
2022-10-13 01:55:43 +0600

    init

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+	return (
+		<>
+			<Head>
+				<title>MineFlayer Bot</title>
+				<meta name='description' content='Made by Rashed Ahmed' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+			<div className=''>hi</div>
+		</>
+	)
+}
